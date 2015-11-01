f2ac576eba4d73f65b16c86918012e865f00bedf
Kyle Mayes <kyle@mayeses.com>
2015-11-01 13:15:56 -0500

    Add easy_plugin plugin stub

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+#![feature(plugin_registrar, rustc_private)]
+
+extern crate rustc;
+extern crate syntax;
+
+use rustc::plugin::{Registry};
+
+use syntax::ast::{TokenTree};
+use syntax::codemap::{Span};
+use syntax::ext::base::{ExtCtxt, DummyResult, MacResult};
+
+/// A result type suitable for reporting errors in plugins.
+pub type PluginResult<T> = Result<T, (Span, String)>;
+
+#[doc(hidden)]
+pub fn easy_plugin(
+    context: &mut ExtCtxt, span: Span, arguments: &[TokenTree]
+) -> PluginResult<Box<MacResult + 'static>> {
+    Ok(DummyResult::any(span))
+}
+
+#[doc(hidden)]
+pub fn expand_easy_plugin(
+    context: &mut ExtCtxt, span: Span, arguments: &[TokenTree]
+) -> Box<MacResult + 'static> {
+    match easy_plugin(context, span, arguments) {
+        Ok(result) => result,
+        Err((span, message)) => {
+            context.span_err(span, &message);
+            DummyResult::any(span)
+        },
+    }
+}
+
+#[doc(hidden)]
+#[plugin_registrar]
+pub fn plugin_registrar(registry: &mut Registry) {
+    registry.register_macro("easy_plugin", expand_easy_plugin);
+}
