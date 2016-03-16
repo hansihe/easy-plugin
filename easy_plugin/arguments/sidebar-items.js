79c67744c95c48d3a4f36aa4b21697ca305c78de
Kyle Mayes <kyle@mayeses.com>
2016-03-16 15:51:08 -0400

    Update documentation

--- easy_plugin/arguments/sidebar-items.js
+++ easy_plugin/arguments/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Match","A plugin argument that has been matched with a named specifier."]],"fn":[["parse_arguments","Parses the given arguments with the given specification."]]});
+initSidebarItems({"enum":[["Match","A plugin argument that has been matched with a named specifier."]],"fn":[["parse_arguments","Parses the supplied arguments with the supplied specification."]]});
