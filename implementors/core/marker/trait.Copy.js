3b526d66552477fd86f6570ce583473d8483dce0
Kyle Mayes <kyle@mayeses.com>
2016-04-05 21:17:14 -0400

    Update documentation

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='easy_plugin/enum.Amount.html' title='easy_plugin::Amount'>Amount</a>",];
+implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='easy_plugin/enum.Amount.html' title='easy_plugin::Amount'>Amount</a>",];implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='easy_plugin/enum.Amount.html' title='easy_plugin::Amount'>Amount</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/trait.Deref.js
+++ implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='easy_plugin/struct.Specification.html' title='easy_plugin::Specification'>Specification</a>",];
+implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='easy_plugin/struct.Specification.html' title='easy_plugin::Specification'>Specification</a>",];implementors['easy_plugin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='easy_plugin/struct.Specification.html' title='easy_plugin::Specification'>Specification</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
